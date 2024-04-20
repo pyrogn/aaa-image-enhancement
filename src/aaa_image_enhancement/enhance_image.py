d19363cd82b33d94b187a67179d697dcfbd69a33
Pavel <60060559+pyrogn@users.noreply.github.com>
2024-04-20 23:03:44 +0300

    Restructure defects detector and enhancer (not tested)

--- src/aaa_image_enhancement/enhance_image.py
+++ src/aaa_image_enhancement/enhance_image.py
@@ -1,61 +1,70 @@
+from dataclasses import fields
+from typing import Protocol
 import numpy as np
 import cv2
+from cv2.typing import MatLike
 import image_dehazer
 from aaa_image_enhancement.exposure_enhancement import enhance_image_exposure
 from aaa_image_enhancement.image_defects_detection import ImageDefects
 
 
-def deblur_image(image, sharpen_strength=9):
-    # https://stackoverflow.com/a/58243090
-    sharpen_kernel = np.array([[-1, -1, -1], [-1, sharpen_strength, -1], [-1, -1, -1]])
-    sharpened_image = cv2.filter2D(image, -1, sharpen_kernel)
-    return sharpened_image
+class ImageEnhancer(Protocol):
+    def __init__(self, img: np.ndarray, defects: ImageDefects) -> None: ...
+    def enhance_image(self) -> np.ndarray: ...
 
 
-def dehaze_image(image, C0=50, C1=500):
-    # https://github.com/Utkarsh-Deshmukh/Single-Image-Dehazing-Python/tree/master
-    return image_dehazer.remove_haze(image, showHazeTransmissionMap=False, C0=C0, C1=C1)
+class ImageEnhancerOpenCV:
+    def __init__(self, img: np.ndarray, defects: ImageDefects) -> None:
+        self.img = img
+        self.defects = defects
+        self.map_defect_fn = {
+            "blur": self.deblur_image,
+            "noisy": self.dehaze_image,
+            "poor_white_balance": self.enhance_wb_image,
+            "low_light": self.enhance_low_light,
+        }
 
+    def deblur_image(self, image, sharpen_strength=9) -> MatLike:
+        # https://stackoverflow.com/a/58243090
+        sharpen_kernel = np.array(
+            [[-1, -1, -1], [-1, sharpen_strength, -1], [-1, -1, -1]]
+        )
+        sharpened_image = cv2.filter2D(image, -1, sharpen_kernel)
+        return sharpened_image
 
-def enhance_wb_image(image, p=0.2, clip_limit=1, tile_grid_size=(8, 8)):
-    wb = cv2.xphoto.createSimpleWB()
-    wb.setP(p)
-    white_balanced_image = wb.balanceWhite(image)
+    def dehaze_image(self, image, C0=50, C1=500) -> MatLike:
+        # https://github.com/Utkarsh-Deshmukh/Single-Image-Dehazing-Python/tree/master
+        return image_dehazer.remove_haze(
+            image, showHazeTransmissionMap=False, C0=C0, C1=C1
+        )[
+            0
+        ]  # should I index it?
 
-    lab = cv2.cvtColor(white_balanced_image, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-    enhanced_l = clahe.apply(l)
-    enhanced_lab = cv2.merge((enhanced_l, a, b))
-    enhanced_image = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
+    def enhance_wb_image(
+        self, image, p=0.2, clip_limit=1, tile_grid_size=(8, 8)
+    ) -> MatLike:
+        wb = cv2.xphoto.createSimpleWB()
+        wb.setP(p)
+        white_balanced_image = wb.balanceWhite(image)
 
-    return enhanced_image
+        lab = cv2.cvtColor(white_balanced_image, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
+        enhanced_l = clahe.apply(l)
+        enhanced_lab = cv2.merge((enhanced_l, a, b))
+        enhanced_image = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
 
+        return enhanced_image
 
-def enhance_low_light(image, gamma=0.6, lambda_=0.15):
-    # https://github.com/pvnieo/Low-light-Image-Enhancement/tree/master
-    return enhance_image_exposure(image, gamma=gamma, lambda_=lambda_)
+    def enhance_low_light(self, image, gamma=0.6, lambda_=0.15) -> np.ndarray:
+        # https://github.com/pvnieo/Low-light-Image-Enhancement/tree/master
+        return enhance_image_exposure(image, gamma=gamma, lambda_=lambda_)
 
-
-map_defect_fn = {
-    ImageDefects.blur: deblur_image,
-    ImageDefects.noisy: dehaze_image,
-    ImageDefects.poor_white_balance: enhance_wb_image,
-    ImageDefects.low_light: enhance_low_light,
-}
-
-
-def enhance_image(image, defects: ImageDefects, **kwargs):
-    for defect in defects:
-        if defect:
-            enhancement_fn = map_defect_fn[defect]
-            enhancement_kwargs = {
-                k: v
-                for k, v in kwargs.items()
-                # crazy thing, rewrite this
-                if k in enhancement_fn.__code__.co_varnames
-            }
-            enhanced_image = enhancement_fn(image, **enhancement_kwargs)
-            return enhanced_image
-    print("no enhancement required")
-    return image
+    def enhance_image(self) -> np.ndarray:
+        for defect in fields(self.defects):
+            if getattr(self.defects, defect.name):
+                enhancement_fn = self.map_defect_fn[defect.name]
+                enhanced_image = enhancement_fn(self.img)
+                return np.array(enhanced_image)
+        print("no enhancement required")
+        return self.img
--- src/aaa_image_enhancement/image_defects_detection.py
+++ src/aaa_image_enhancement/image_defects_detection.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
 import cv2
 import numpy as np
 from skimage.exposure import is_low_contrast as ski_is_low_contrast
 from skimage.restoration import estimate_sigma
-from typing import NamedTuple
+from typing import NamedTuple, Protocol
 from PIL import Image, ImageChops
 
 
@@ -14,12 +15,31 @@ from PIL import Image, ImageChops
 # потом на их основе определять дефекты (bool)
 
 
-class ImageProperties:
-    def __init__(self, image):
-        self.image = image
-        self.gray = self.to_gray(image)
-        self.trimmed_image = self.trim(self.cv2_to_pil(image))
+@dataclass
+class ImageDefects:
+    blur: bool = False
+    low_light: bool = False
+    low_contrast: bool = False
+    poor_white_balance: bool = False
+    noisy: bool = False
+
+
+# Почитать про протокол
+# https://idego-group.com/blog/2023/02/21/we-need-to-talk-about-protocols-in-python/
+class DefectsDetector(Protocol):
+    def __init__(self, img: np.ndarray, **kwargs) -> None:
+        "RGB image"
+
+    def find_defects(self) -> ImageDefects: ...
+
+
+class DefectsDetectorOpenCV:
+    def __init__(self, img: np.ndarray, **kwargs) -> None:
+        self.img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        self.gray = self.to_gray(img)
+        self.trimmed_image = self.trim(self.cv2_to_pil(img))
         self.trimmed_gray = self.to_gray(self.pil_to_cv2(self.trimmed_image))
+        self.params = kwargs
 
     @staticmethod
     def to_gray(image):
@@ -47,12 +67,12 @@ class ImageProperties:
     def is_noisy(self, threshold=2.0) -> bool:  # review
         sigma = estimate_sigma(self.trimmed_gray, average_sigmas=True)
 
-        return sigma > threshold
+        return sigma > threshold  # type: ignore
 
     def is_blurry(self, threshold=100.0) -> bool:
         blur_map = cv2.Laplacian(self.trimmed_gray, cv2.CV_64F)
         score = np.var(blur_map)
-        return score < threshold
+        return score < threshold  # type: ignore
 
     def is_low_light(self, threshold=80) -> bool:
         avg_intensity = np.mean(self.trimmed_gray)
@@ -63,34 +83,23 @@ class ImageProperties:
 
     def is_poor_white_balance(self):  # review
         b, g, r = cv2.split(self.pil_to_cv2(self.trimmed_image))
-        avg_b, avg_g, avg_r = np.mean(b), np.mean(g), np.mean(r)
+        avg_b, avg_g, avg_r = np.mean(b), np.mean(g), np.mean(r)  # type: ignore
         return (
             abs(avg_b - avg_g) > 20
             or abs(avg_b - avg_r) > 20
             or abs(avg_g - avg_r) > 20
         )
 
-
-class ImageDefects(NamedTuple):
-    blur: bool
-    low_light: bool
-    low_contrast: bool
-    poor_white_balance: bool
-    noisy: bool
-
-
-def detect_image_defects(
-    image_properties: ImageProperties,
-    blur_threshold=100,
-    low_light_threshold=80,
-    contrast_threshold=0.35,
-    noise_threshold=2.0,
-) -> ImageDefects:
-    defects = ImageDefects(
-        blur=image_properties.is_blurry(blur_threshold),
-        low_light=image_properties.is_low_light(low_light_threshold),
-        low_contrast=image_properties.is_low_contrast(contrast_threshold),
-        poor_white_balance=image_properties.is_poor_white_balance(),
-        noisy=image_properties.is_noisy(noise_threshold),
-    )
-    return defects
+    def find_defects(self) -> ImageDefects:
+        defects = ImageDefects()
+        # maybe add feature to selectively apply detectors?
+        defects.blur = self.is_blurry(self.params.get("blur_threshold", 100.0))
+        defects.low_light = self.is_low_light(
+            self.params.get("low_light_threshold", 80)
+        )
+        defects.low_contrast = self.is_low_contrast(
+            self.params.get("low_contrast_threshold", 0.35)
+        )
+        defects.poor_white_balance = self.is_poor_white_balance()
+        defects.noisy = self.is_noisy(self.params.get("noisy_threshold", 2.0))
+        return defects
